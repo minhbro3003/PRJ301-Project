886c0baebd77c1178e81a85d07806f2e767bb55e
NguyenVanMinh <minhtiencool03@gmail.com>
2024-03-04 17:42:29 +0700

    Create DBContext.java

---
+++ My_Project_PRJ301/src/java/dal/DBContext.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author PC
+ */
+public abstract class DBContext<T> {
+
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            String url = "jdbc:sqlserver://localhost\\MSSQLSERVER:1433;databaseName=AssDemo; encrypt=true;trustServerCertificate=true;";
+            String user = "sa";
+            String pass = "minh";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch(SQLException ex){
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+    public abstract ArrayList<T> list();
+}
