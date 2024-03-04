ec2d4747da61d9c689a49cb9203fa7f622da6581
NguyenVanMinh <minhtiencool03@gmail.com>
2024-03-05 01:19:36 +0700

    Create GradeDBContext.java

---
+++ My_Project_PRJ301/src/java/dal/GradeDBContext.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import entity.Grade;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PC
+ */
+public class GradeDBContext extends DBContext<Grade>{
+
+    
+    
+    @Override
+    public ArrayList<Grade> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
