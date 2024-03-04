01c884257b0898ed79162374dd9732654b42d904
NguyenVanMinh <minhtiencool03@gmail.com>
2024-03-04 18:39:00 +0700

    Create Grade.java
    Class Grade

---
+++ My_Project_PRJ301/src/java/entity/Grade.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author PC
+ */
+public class Grade {
+    private int id;
+    private Exame exame;
+    private Student student;
+    private float score;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Exame getExame() {
+        return exame;
+    }
+
+    public void setExame(Exame exame) {
+        this.exame = exame;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+    
+    
+}
