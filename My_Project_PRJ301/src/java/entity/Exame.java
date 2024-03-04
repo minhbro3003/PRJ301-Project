f26075abae986b2e64dd8ff99843f41cc218777e
NguyenVanMinh <minhtiencool03@gmail.com>
2024-03-04 18:35:23 +0700

    Create Exame.java
    Class Exame

---
+++ My_Project_PRJ301/src/java/entity/Exame.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+import java.util.Date;
+
+/**
+ *
+ * @author PC
+ */
+public class Exame {
+    private int id;
+    private Assessment assessment;
+    private Date starDate;
+    private Date endDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Assessment getAssessment() {
+        return assessment;
+    }
+
+    public void setAssessment(Assessment assessment) {
+        this.assessment = assessment;
+    }
+
+    public Date getStarDate() {
+        return starDate;
+    }
+
+    public void setStarDate(Date starDate) {
+        this.starDate = starDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    
+    
+}
